4ba34a549af25a7b00fccd8977e2d2428c8b6787
Gaurav Lohkna <44726964+lohkna007@users.noreply.github.com>
2024-11-21 20:25:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,3 @@ document.querySelector('.hamburger').addEventListener('click', () => {
     const navMenu = document.querySelector('nav ul');
     navMenu.classList.toggle('active');
 });
-
-
-// Highlight the current section in the navigation bar
-window.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('nav ul li a');
-
-    let currentSection = '';
-    
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - 80;
-        if (scrollY >= sectionTop) {
-            currentSection = section.getAttribute('id');
-        }
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href').includes(currentSection)) {
-            link.classList.add('active');
-        }
-    });
-});
